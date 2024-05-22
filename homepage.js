696f4ef8b3e5b669a36e72b65f9c9ee2f3d5be05
Cam-com <camille@plushg.fr>
2024-05-22 13:29:12 +0200

    Initial commit

---
+++ homepage.js
@@ -0,0 +1,6 @@
+//! Hellooooo Let's do a good job
+// Im not sure where I'll need javascript in this website
+// But this is fun
+const Yay = grrrr 
+
+//How do we even integrate Javascript to a web page ???
